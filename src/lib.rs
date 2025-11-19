cb657cd51701e6db7c926679352ca66d534abe3e
Michalis Molfetas <mixalismolfetas@gmail.com>
2025-11-19 04:57:49 +0200

    feat(alloc): add allocation tagging for memfd/malloc distinction

--- src/lib.rs
+++ src/lib.rs
@@ -50,12 +50,30 @@ pub enum Error {
    NullAllocation,
 }
 
-
 #[cfg(all(feature = "use_os", unix))]
 static mut SUPPORTS_MEMFD_SECRET: bool = false;
 #[cfg(all(feature = "use_os", unix))]
 static SUPPORTS_MEMFD_SECRET_INIT: Once = Once::new();
+#[cfg(all(feature = "use_os", unix))]
+const ALLOC_TAG_MALLOC: usize = 0xDEAD_BEEF;
+#[cfg(all(feature = "use_os", unix))]
+const ALLOC_TAG_MEMFD: usize = 0x5EC0_0000;
 
+/// Calculates the offset needed to store a usize header while maintaining
+/// the alignment requirements of T.
+#[cfg(all(feature = "use_os", unix))]
+const fn get_header_offset<T>() -> usize {
+   let header_size = core::mem::size_of::<usize>();
+   let align = core::mem::align_of::<T>();
+
+   // If T needs stronger alignment than usize, we must pad more.
+   // Otherwise, sizeof(usize) is sufficient.
+   if align > header_size {
+      align
+   } else {
+      header_size
+   }
+}
 
 #[cfg(all(feature = "use_os", unix))]
 unsafe fn supports_memfd_secret_init() {
@@ -73,7 +91,7 @@ unsafe fn supports_memfd_secret_init() {
       if errno == ENOSYS {
          // not supported
       } else {
-         // Other error treat as unsupported
+         // Other error
       }
        */
    }
@@ -100,37 +118,69 @@ pub(crate) unsafe fn alloc<T>(size: usize) -> Result<NonNull<T>, Error> {
       #[cfg(unix)]
       {
          SUPPORTS_MEMFD_SECRET_INIT.call_once(|| unsafe { supports_memfd_secret_init() });
-
          let supports_memfd_secret = unsafe { SUPPORTS_MEMFD_SECRET };
 
+         let header_offset = get_header_offset::<T>();
+
+         // Calculate alignment requirement
+         let align_req = core::mem::align_of::<usize>().max(core::mem::align_of::<T>());
+
+         // Calculate raw size (Header + Data)
+         let raw_size = size
+            .checked_add(header_offset)
+            .ok_or(Error::AllocationFailed)?;
+
+         // Calculate padded size to satisfy alignment
+         let remainder = raw_size % align_req;
+         let alloc_size = if remainder == 0 {
+            raw_size
+         } else {
+            raw_size
+               .checked_add(align_req - remainder)
+               .ok_or(Error::AllocationFailed)?
+         };
+
          let ptr_opt = if supports_memfd_secret {
-            unsafe { memsec::memfd_secret_sized(size) }
+            unsafe { memsec::memfd_secret_sized(alloc_size) }
          } else {
             None
          };
 
-         if let Some(ptr) = ptr_opt {
-            NonNull::new(ptr.as_ptr() as *mut T).ok_or(Error::NullAllocation)
-         } else {
-            unsafe {
-               let allocated_ptr = memsec::malloc_sized(size);
-               let non_null = allocated_ptr.ok_or(Error::AllocationFailed)?;
-               let ptr = non_null.as_ptr() as *mut T;
-               NonNull::new(ptr).ok_or(Error::NullAllocation)
-            }
+         if let Some(raw_ptr_nonnull) = ptr_opt {
+            let raw_ptr = raw_ptr_nonnull.as_ptr() as *mut u8;
+
+            // Write the MEMFD tag
+            unsafe { *(raw_ptr as *mut usize) = ALLOC_TAG_MEMFD };
+
+            let user_ptr = unsafe { raw_ptr.add(header_offset) as *mut T };
+            return NonNull::new(user_ptr).ok_or(Error::NullAllocation);
+         }
+
+         unsafe {
+            let allocated_ptr = memsec::malloc_sized(alloc_size);
+            let non_null = allocated_ptr.ok_or(Error::AllocationFailed)?;
+
+            let raw_ptr = non_null.as_ptr() as *mut u8;
+
+            // Write the MALLOC tag
+            *(raw_ptr as *mut usize) = ALLOC_TAG_MALLOC;
+
+            let user_ptr = raw_ptr.add(header_offset) as *mut T;
+            NonNull::new(user_ptr).ok_or(Error::NullAllocation)
          }
       }
    }
 
    #[cfg(not(feature = "use_os"))]
    {
+      use core::alloc::Layout;
       let layout =
          Layout::from_size_align(size, mem::align_of::<T>()).map_err(|_| Error::AlignmentFailed)?;
       let ptr = unsafe { alloc::alloc(layout) as *mut T };
       if ptr.is_null() {
          return Err(Error::NullAllocation);
       }
-      ptr
+      unsafe { NonNull::new_unchecked(ptr) }
    }
 }
 
@@ -143,27 +193,57 @@ pub(crate) fn free<T>(ptr: NonNull<T>) {
 
    #[cfg(unix)]
    {
-      let supports_memfd_secret = unsafe { SUPPORTS_MEMFD_SECRET };
-      if supports_memfd_secret {
-         unsafe { memsec::free_memfd_secret(ptr) };
-      } else {
-         unsafe { memsec::free(ptr) };
+      let header_offset = get_header_offset::<T>();
+
+      unsafe {
+         let user_ptr = ptr.as_ptr() as *mut u8;
+         let raw_ptr = user_ptr.sub(header_offset);
+
+         // Reconstruct the NonNull pointer to the START of the allocation (header)
+         let non_null_raw = NonNull::new_unchecked(raw_ptr);
+
+         // Read the tag
+         let tag = *(raw_ptr as *const usize);
+
+         match tag {
+            ALLOC_TAG_MEMFD => {
+               memsec::free_memfd_secret(non_null_raw);
+            }
+            ALLOC_TAG_MALLOC => {
+               memsec::free(non_null_raw);
+            }
+            _ => {
+               // SHOULD NOT HAPPEN
+               // Tag mismatch: Double free or corruption.
+               #[cfg(debug_assertions)]
+               panic!(
+                  "SecureAllocator: Corrupt header tag found: {:x}",
+                  tag
+               );
+            }
+         }
       }
    }
 }
 
 #[cfg(feature = "use_os")]
 pub(crate) fn mprotect<T>(ptr: NonNull<T>, prot: Prot::Ty) -> bool {
-   let success = unsafe { memsec::mprotect(ptr, prot) };
-   #[cfg(test)]
+   #[cfg(unix)]
    {
-      if !success {
-         eprintln!("mprotect failed");
+      // We need to protect the whole block, including the header.
+      let header_offset = get_header_offset::<T>();
+      unsafe {
+         let raw_ptr = (ptr.as_ptr() as *mut u8).sub(header_offset);
+         let raw_non_null = NonNull::new_unchecked(raw_ptr as *mut T);
+
+         memsec::mprotect(raw_non_null, prot)
       }
    }
-   success
+   #[cfg(windows)]
+   {
+      unsafe { memsec::mprotect(ptr, prot) }
+   }
 }
-
 
 #[cfg(test)]
 mod tests {
